bb78ddf5a37db5b64d57949aa233e72c9e42fba6
Danny Thomas <dannyt@netflix.com>
2017-10-19 12:06:20 -0700

    Fix deprecation warnings

--- src/main/kotlin/nebula/plugin/compile/JavaCrossCompilePlugin.kt
+++ src/main/kotlin/nebula/plugin/compile/JavaCrossCompilePlugin.kt
@@ -1,13 +1,17 @@
 package nebula.plugin.compile
 
+import com.netflix.nebula.interop.versionGreaterThan
+import com.netflix.nebula.interop.versionLessThan
 import nebula.plugin.compile.provider.DefaultLocationJDKPathProvider
 import nebula.plugin.compile.provider.EnvironmentJDKPathProvider
 import nebula.plugin.compile.provider.SDKManJDKPathProvider
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
 import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.compile.CompileOptions
 import org.gradle.api.tasks.compile.GroovyCompile
 import org.gradle.api.tasks.compile.JavaCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -15,6 +19,7 @@ import org.jetbrains.kotlin.utils.addToStdlib.firstNotNullResult
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
+import kotlin.reflect.full.memberFunctions
 
 class JavaCrossCompilePlugin : Plugin<Project> {
     companion object {
@@ -38,12 +43,22 @@ class JavaCrossCompilePlugin : Plugin<Project> {
         val targetCompatibility = convention.targetCompatibility
         if (targetCompatibility != JavaVersion.current()) {
             with(project.tasks) {
-                val location by lazy { targetCompatibility.locate() }
+                val location by lazy { targetCompatibility.locate(project) }
+                val bootstrapClasspath = location.bootstrapClasspath
+                val bootClasspath = bootstrapClasspath.joinToString(File.pathSeparator)
                 withType(JavaCompile::class.java) {
-                    it.options.bootClasspath = location.bootClasspath
+                    if (project.gradle.versionGreaterThan("4.2.1")) {
+                        it.options.bootstrapClasspath = bootstrapClasspath
+                    } else {
+                        it.options.bootClasspath = bootClasspath
+                    }
                 }
                 withType(GroovyCompile::class.java) {
-                    it.options.bootClasspath = location.bootClasspath
+                    if (project.gradle.versionGreaterThan("4.2.1")) {
+                        it.options.bootstrapClasspath = bootstrapClasspath
+                    } else {
+                        it.options.bootClasspath = bootClasspath
+                    }
                 }
                 project.plugins.withId("kotlin") {
                     withType(KotlinCompile::class.java) {
@@ -54,7 +69,7 @@ class JavaCrossCompilePlugin : Plugin<Project> {
         }
     }
 
-    private fun JavaVersion.locate(): JavaLocation {
+    private fun JavaVersion.locate(project: Project): JavaLocation {
         logger.debug("Locating JDK for $this")
         val jdkHome = providers
                 .firstNotNullResult {
@@ -85,10 +100,10 @@ class JavaCrossCompilePlugin : Plugin<Project> {
         val libDir = runtimeJar.parentFile
         val jarFiles = listOf(runtimeJar) + ADDITIONAL_JARS.map { File(libDir, "$it.jar") }
         val classpath = jarFiles.joinToString(File.pathSeparator)
-        return JavaLocation(jdkHome, classpath)
+        return JavaLocation(jdkHome, project.files(classpath))
     }
 
     private fun JavaVersion.cannotLocate(): IllegalStateException = IllegalStateException("Could not locate a compatible JDK for target compatibility $this. Change the source/target compatibility, set a JDK_1$majorVersion environment variable with the location, or install to one of the default search locations")
 
-    data class JavaLocation(val jdkHome: String, val bootClasspath: String)
+    data class JavaLocation(val jdkHome: String, val bootstrapClasspath: FileCollection)
 }
