3b72ce18b7da82503ab6fcbfff41fd748f27286b
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-07-08 19:36:53 -0700

    SDKManJDKPathProvider should use configuration cache to get user.home

--- src/main/kotlin/nebula/plugin/compile/provider/EnvironmentJDKPathProvider.kt
+++ src/main/kotlin/nebula/plugin/compile/provider/EnvironmentJDKPathProvider.kt
@@ -1,12 +1,20 @@
 package nebula.plugin.compile.provider
 
+import com.netflix.nebula.interop.versionLessThan
 import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.provider.ProviderFactory
 
 /**
  * Provide JDK path from environment variables.
  */
-class EnvironmentJDKPathProvider : JDKPathProvider {
+class EnvironmentJDKPathProvider constructor(private val providerFactory: ProviderFactory, private val project: Project) : JDKPathProvider {
     override fun provide(javaVersion: JavaVersion): String? {
-        return System.getenv("JDK_1${javaVersion.majorVersion}")
+        return if(project.gradle.versionLessThan("6.5")) {
+            val jdkEnvVariable = providerFactory.environmentVariable("JDK_1${javaVersion.majorVersion}").forUseAtConfigurationTime()
+            if(jdkEnvVariable.isPresent) jdkEnvVariable.get() else null
+        } else {
+            System.getenv("JDK_1${javaVersion.majorVersion}")
+        }
     }
 }
